3c1596f33aa25375b7c9a33aa8d65b16b8057010
Sasank Madineni <sasank1020@gmail.com>
2025-09-11 14:44:53 -0700

    added sideplayer class

---
+++ Scripts/Player/SidePlayer.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+namespace Bookworm.Entity;
+
+public partial class SidePlayer : CharacterBody2D
+{
+
+    public override void _Ready()
+    {
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+
+}
